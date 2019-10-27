d27f65b464a61105bc48f949819592d567b6e601
Alexandru Rosianu <me@aluxian.com>
2019-10-27 20:27:38 +0000

    customise

--- src/index.js
+++ src/index.js
@@ -1,87 +1,30 @@
+process.on('uncaughtException', (ex) => console.log(ex));
+
 const path = require('path')
 const fs = require('fs')
 const AdmZip = require('adm-zip')
 const formatDate = require('date-fns').format
-const moment = require('moment')
+const moment = require('moment-timezone')
 var sanitize = require('sanitize-filename')
 const { ensureDirectoriesExist } = require('./utils')
 
-getDayoneFolderPath()
-    .then(getDayoneZips)
-    .then(unpackDayone)
-    .catch(err => {
-        throw new Error(err)
-    })
+console.log('Reading journal file:', process.argv[2])
+unpackDayone(process.argv[2])
 
-function getDayoneFolderPath() {
-    return new Promise((res, reject) => {
-        const dayonePath = path.resolve('dayone')
-
-        fs.stat(dayonePath, (err, stat) => {
-            if (err || !stat.isDirectory()) {
-                return reject('No `dayone` folder found.`')
-            }
-            return res(dayonePath)
-        })
-    })
-}
-
-function getDayoneZips(dayonePath) {
-    return new Promise((res, reject) => {
-        fs.readdir(dayonePath, (err, files) => {
-            if (err) {
-                return reject(err)
-            }
-            const zipFiles = files.reduce((prev, current) => {
-                if (current.indexOf('.zip') === current.length - 4) {
-                    prev.push(path.join(dayonePath, current))
-                }
-                return prev
-            }, [])
-            if (!zipFiles.length) {
-                return reject('No zip files found in `dayone` folder.')
-            }
-
-            res(zipFiles)
-        })
-    })
-}
-
-function unpackDayone(zipDirectories) {
+function unpackDayone(jsonPath) {
     let entriesProcess = 0
 
-    zipDirectories.map(unpack)
+    // Make sure the output directories exist
+    const entriesDirectory = path.resolve('./entries')
+    ensureDirectoriesExist([entriesDirectory])
 
-    function unpack(directory) {
-        // Make sure the output directories exist
-        const entriesDirectory = path.resolve('./src/entries')
-        const photosDirectory = path.resolve('./public/static')
-        ensureDirectoriesExist([photosDirectory, entriesDirectory])
+    // Main json file
+    const fullData = JSON.parse(fs.readFileSync(jsonPath, 'utf8'))
+    const mdEntries = fullData.entries.map(entryToMarkdown)
 
-        var zip = new AdmZip(directory)
-        var zipEntries = zip.getEntries() // an array of ZipEntry records
+    entriesProcess += mdEntries.length
 
-        zipEntries.forEach(function(zipEntry) {
-            // Main json file
-            if (zipEntry.entryName === 'Journal.json') {
-                const fullData = JSON.parse(zip.readAsText(zipEntry.entryName))
-                const mdEntries = fullData.entries.map(entryToMarkdown)
-
-                entriesProcess += mdEntries.length
-
-                saveEntries(mdEntries, entriesDirectory)
-            }
-            // Photos
-            if (zipEntry.entryName.indexOf('photos/') === 0) {
-                zip.extractEntryTo(
-                    /*entry name*/ zipEntry.entryName,
-                    /*target path*/ photosDirectory,
-                    /*maintainEntryPath*/ false,
-                    /*overwrite*/ true
-                )
-            }
-        })
-    }
+    saveEntries(mdEntries, entriesDirectory)
 
     console.log(`${entriesProcess} entries processed!`)
 }
@@ -100,40 +43,92 @@ function entryToMarkdown(entry) {
     }
 
     let formattedEntry = {
-        id: entry.uuid,
-        date: formatDate(new Date(entry.creationDate), 'D MMM, YYYY'),
+        //id: entry.uuid,
         title,
         location: entry.location ? entry.location.placeName : '',
-        starred: entry.starred,
-        tags: entry.tags
+        latitude: entry.location ? entry.location.latitude : '',
+        longitude: entry.location ? entry.location.longitude : '',
+      tz: entry.timeZone,
+        //weather: entry.weather ? entry.weather.conditionsDescription : '',
+        // starred: entry.starred,
+        // tags: entry.tags
     }
 
-    formattedEntry.md = [getFrontMatter(formattedEntry), text].join('\n\n')
+    formattedEntry.md = [getFrontMatter(formattedEntry), text.replace(/\\/g, '')].join('\n\n')
+    formattedEntry.date = new Date(entry.creationDate)
 
     return formattedEntry
 }
 
 function saveEntries(entries, location) {
-    entries.map(({ md, date, title }) => {
-        const fileDate = moment(date, 'D MMM, YYYY').format('YYYY-MM-DD')
-        const fileName = sanitize(title)
-        var saveTo = path.join(location, `${fileDate} - ${fileName}.md`)
-        fs.writeFileSync(saveTo, md, {
+    entries.map(({ md, date, title,tz }) => {
+        let fileName = sanitize(title).substring(2).trim()
+        fileName = moment(date).tz(tz).format('YYYY-MM-DD') + ' ' + fileName
+/*
+        if (/^# \d{4}\-\d{2}\-/.test(fileName)) {
+          fileName = fileName.substring(2)
+          if (fileName[9] === ' ')
+            fileName = fileName.substring(0, 8) + '0' + fileName.substring(8)
+          if (fileName[13] ==='-')
+            fileName = fileName.substring(0, 13) + '' + fileName.substring(14)
+        } else {
+          fileName = moment(date).tz(tz).format('YYYY-MM-DD HHmm')
+        }
+
+        var saveTo = path.join(location, `${fileName}.md`)
+      var is59=false
+        if (fs.existsSync(saveTo)) {
+          console.log('path exists, changing from', fileName)
+          if (moment(fileName, 'YYYY-MM-DD HHmm').minutes() !== 59){
+            newsecs = moment(fileName, 'YYYY-MM-DD HHmm').minutes() + 1
+            fileName = moment(fileName, 'YYYY-MM-DD HHmm').minutes(newsecs).format('YYYY-MM-DD HHmm')
+            console.log('to ', fileName, newsecs)
+          } else {
+            console.log('59')
+            is59=true
+          }
+        }
+
+        if (is59 && fs.existsSync(saveTo)) {
+          console.log('path exists, changing from', fileName)
+          if (moment(fileName, 'YYYY-MM-DD HHmm').hours() !==23){
+            newsecs = moment(fileName, 'YYYY-MM-DD HHmm').hours() + 1
+            fileName = moment(fileName, 'YYYY-MM-DD HHmm').hours(newsecs).minutes(0).format('YYYY-MM-DD HHmm')
+            console.log('to ', fileName, newsecs)
+          } else {
+            console.log('23')
+          }
+        }
+
+        if (fileName.length !== 15)
+        console.log('filename does not have 15 len', fileName)
+*/
+        let fileDate2 = moment(date).tz(tz).format('DD/MM/YYYY HH:mm:ss')
+
+        saveTo = path.join(location, `${fileName}.md`)
+        if (fs.existsSync(saveTo)) {
+          console.log('already exists', saveTo)
+          saveTo += '.DUPE.' + (Math.random()) + '.md'
+        }
+        fs.writeFileSync(saveTo, md.trim(), {
             encoding: 'utf8'
         })
+        cp=require('child_process')
+        my_cmd=`SetFile -d "${fileDate2}" "${saveTo}"`
+        console.log('running', my_cmd)
+        cp.execSync(my_cmd)
     })
 }
 
 function getFrontMatter(entry) {
-    var frontMatter = '---\n'
-    for (var prop in entry) {
-        if (entry.hasOwnProperty(prop)) {
-            let item = entry[prop] || ''
-            if (Array.isArray(item)) {
-                item = item.join(', ')
-            }
-            frontMatter = `${frontMatter}${prop}: ${JSON.stringify(item)}\n`
-        }
-    }
-    return frontMatter + '---'
+    mt = `${entry.title.replace(/\s+/g, ' ').replace(/\\/g, '')}`
+    mt = mt.substring(2).trim()
+    /*if (mt.startsWith('#')) {
+       firstBlank = mt.indexOf(' ')
+      tag = mt.substring(0, firstBlank)
+      mt = mt.substring(firstBlank+1).trim()
+      mt = mt + '\n' + tag
+    }*/
+  return mt.trim()
+
 }
